302cdf75ca2241da3787a1301792ed34d9972aa8
eis <eis@iki.fi>
2012-12-04 18:38:33 +0200

    lookup works, but not in a blueprint way

--- osgi-web-app/src/main/java/fi/eis/applications/jboss/poc/wab/AnotherHelloWorldServlet.java
+++ osgi-web-app/src/main/java/fi/eis/applications/jboss/poc/wab/AnotherHelloWorldServlet.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.jboss.logging.Logger;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 import fi.eis.applications.jboss.poc.osgiservice.api.MessageService;
 
@@ -36,11 +38,32 @@ public class AnotherHelloWorldServlet extends HttpServlet {
 	  return this.service;
   }
   
+  // TODO use blueprint for this
   @Override
   public void init(ServletConfig config) throws ServletException {
       super.init(config);
       bundleContext = (BundleContext) config.getServletContext().getAttribute("osgi-bundlecontext");
       
+      final HttpServlet servlet = this;
+      
+      log.debugf("Registering with parameters %s, %s, %s", bundleContext, MessageService.class.getName(), null);
+      
+      ServiceTracker serviceTracker = new ServiceTracker(bundleContext, MessageService.class.getName(), null){
+          @Override
+          public Object addingService(final ServiceReference sref) {
+            log.infof("Adding service: %s to %s", sref, servlet);
+            service = (MessageService) super.addingService(sref);
+            return service;
+          }
+
+          @Override
+          public void removedService(final ServiceReference sref, final Object sinst) {
+            super.removedService(sref, service);
+            log.infof("Removing service: %s from %s", sref, servlet);
+            service = null;
+          }    	  
+      };  
+      serviceTracker.open(); 
   }  
 
   static String PAGE_HEADER = "<html><head><title>helloworld</title><body>";
@@ -52,7 +75,7 @@ public class AnotherHelloWorldServlet extends HttpServlet {
     resp.setContentType("text/html");
     PrintWriter writer = resp.getWriter();
     writer.println(PAGE_HEADER);
-    //writer.println("<h1>" + service.getMessage() + "</h1>");
+    writer.println("<h1>" + service.getMessage() + "</h1>");
     writer.print("Using BundleContext looked up in Servlet Context to list available OSGi Bundles:");
 	writer.print("<ul>");
     for (Bundle bundle : bundleContext.getBundles()) {
